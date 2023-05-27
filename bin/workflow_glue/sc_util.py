56603b57594f62d960a1b515e67d95200a0d9f6d
jdemeul <jonas.demeulemeester@kuleuven.be>
2023-05-27 21:15:09 +0200

    back to tn5 bit

--- bin/workflow_glue/sc_util.py
+++ bin/workflow_glue/sc_util.py
@@ -15,6 +15,6 @@ kit_adapters = {
     },
     'multiomeatac': {
         'adapter1': 'CGGCGACCACCGAGATCTACAC',
-        'adapter2': 'TAGAGCATACGGCAGAAGACGAAC'
+        'adapter2': 'TACACATCTCCGAGCCCACGAGAC'
     }
 }
